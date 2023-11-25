3e6b4f030844787c8346e54ff8cdafdd0e53436d
Siyar Yamin <yaminsiyar@yahoo.de>
2023-11-25 17:02:55 +0100

    fix: changed datasource back to prod env

--- src/test/java/at/fhtw/swen3/persistence/repository/DocumentRepositoryTest.java
+++ src/test/java/at/fhtw/swen3/persistence/repository/DocumentRepositoryTest.java
@@ -1,7 +1,6 @@
-package at.fhtw.swen3.persistence;
+package at.fhtw.swen3.persistence.repository;
 
 import at.fhtw.swen3.persistence.entity.Document;
-import at.fhtw.swen3.persistence.repository.DocumentRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
