502ed469c986e1d558200ca9668229c306b33457
L-ynx <raza.gh97@gmail.com>
2024-01-09 01:42:01 +0100

    fix dependency errors

--- PaperlessREST/src/main/java/at/fhtw/swen3/persistence/service/SearchIndexService.java
+++ PaperlessREST/src/main/java/at/fhtw/swen3/persistence/service/SearchIndexService.java
@@ -5,13 +5,8 @@ import at.fhtw.swen3.persistence.entity.Document;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
 public interface SearchIndexService {
-
-    Optional<Document> getDocumentById(int id);
-
-    boolean deleteDocumentById(int id);
 
     List<Document> searchDocument(String string) throws IOException;
 }
--- PaperlessREST/src/main/java/at/fhtw/swen3/persistence/service/SearchIndexServiceImpl.java
+++ PaperlessREST/src/main/java/at/fhtw/swen3/persistence/service/SearchIndexServiceImpl.java
@@ -3,10 +3,8 @@ package at.fhtw.swen3.persistence.service;
 import at.fhtw.swen3.paperless.config.ElasticSearchConfig;
 import at.fhtw.swen3.persistence.entity.Document;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.elasticsearch._types.Result;
-import co.elastic.clients.elasticsearch.core.DeleteResponse;
-import co.elastic.clients.elasticsearch.core.GetResponse;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.Hit;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.Optional;
 import java.util.List;
 
 @Service
@@ -36,37 +33,6 @@ public class SearchIndexServiceImpl implements SearchIndexService {
         }
     }
 
-
-    @Override
-    public Optional<Document> getDocumentById(int id) {
-        try {
-            GetResponse<Document> response = esClient.get(g -> g
-                            .index(ElasticSearchConfig.DOCUMENTS_INDEX_NAME)
-                            .id(String.valueOf(id)),
-                    Document.class
-            );
-            return (response.found() && response.source() != null) ? Optional.of(response.source()) : Optional.empty();
-        } catch (IOException e) {
-            LOGGER.error("Failed to get document id=" + id + " from elasticsearch: " + e);
-            return Optional.empty();
-        }
-    }
-
-    @Override
-    public boolean deleteDocumentById(int id) {
-        DeleteResponse result = null;
-        try {
-            result = esClient.delete(d -> d.index(ElasticSearchConfig.DOCUMENTS_INDEX_NAME).id(String.valueOf(id)));
-        } catch (IOException e) {
-            LOGGER.warn("Failed to delete document id=" + id + " from elasticsearch: " + e);
-        }
-        if (result == null)
-            return false;
-        if (result.result() != Result.Deleted)
-            LOGGER.warn(result.toString());
-        return result.result() == Result.Deleted;
-    }
-
     @Override
     public List<Document> searchDocument(String searchString) throws IOException {
         //search document with query
@@ -78,7 +44,7 @@ public class SearchIndexServiceImpl implements SearchIndexService {
                                     m -> m.fields("content", "title").fuzziness("AUTO").query("*" + searchString + "*")
                             )), Document.class);
 
-            return searchResponse.hits().hits().stream().map(hit -> hit.source()).toList();
+            return searchResponse.hits().hits().stream().map(Hit::source).toList();
         } catch (IOException e) {
             LOGGER.error("Error searching for documents \n" + e.getMessage());
             throw e;
