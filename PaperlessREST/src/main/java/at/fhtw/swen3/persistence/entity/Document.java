781ac71f5b1c4c0777cb94319301b9fbdde497f3
bhinder-if21b107 <if21b107@technikum-wien.at>
2024-01-22 16:13:49 +0100

    added validators to Document Entity class

--- PaperlessREST/src/main/java/at/fhtw/swen3/persistence/entity/Document.java
+++ PaperlessREST/src/main/java/at/fhtw/swen3/persistence/entity/Document.java
@@ -1,6 +1,7 @@
 package at.fhtw.swen3.persistence.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -16,17 +17,31 @@ public class Document {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotNull(message = "Filesize is required")
     private long filesize;
 
     @Column(columnDefinition = "TEXT")
     private String content;
+
+    @NotNull(message = "Title is required")
     private String title;
+
     private String originalName;
+
+    @NotNull(message = "Checksum is required")
     private String checksum;
+
+    @NotNull(message = "MimeType is required")
     private String mimeType;
+
+    @NotNull(message = "StoragePath is required")
     private String storagePath;
+
+    @NotNull(message = "Owner is required")
     private String owner;
 
+    @NotNull(message = "CreatedAt is required")
     private LocalDateTime createdAt;
     private LocalDateTime addedAt;
     private LocalDateTime modified;
